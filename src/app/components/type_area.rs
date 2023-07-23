70fa17072ddfbe9c50adde504f2a8baa5fdd515c
MC <mc@DESKTOP-GH71P6P.localdomain>
2023-07-23 03:41:31 +0200

    style input

--- src/app/components/type_area.rs
+++ src/app/components/type_area.rs
@@ -5,14 +5,17 @@ pub fn TypeArea(cx: Scope, send: Action<String, Result<String, ServerFnError>>) 
   let input_ref = create_node_ref::<Input>(cx);
   view! {cx,
     <div>
-      <form on:submit=move |ev| {
-        ev.prevent_default();
-        let input = input_ref.get().expect("input to exist");
-        send.dispatch(input.value());
-        input.set_value("");
-      }>
-        <input class="chat-input" type="text" node_ref=input_ref/>
-        <input class="submit-btn" type="submit" />
+      <form
+        class="text-form"
+        on:submit=move |ev| {
+          ev.prevent_default();
+          let input = input_ref.get().expect("input to exist");
+          send.dispatch(input.value());
+          input.set_value("");
+        }
+      >
+        <input placeholder="Do anything now!" class="chat-input" type="text" node_ref=input_ref/>
+        <button class="submit-btn" type="submit">"Send"</button>
       </form>
     </div>
   }
